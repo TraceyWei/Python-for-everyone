c913a14a29596c645d92b5e05592880578ba7872
Tracey <xiaohuiw_2000@yahoo.com>
2025-07-09 20:41:21 -0700

    asds

--- Class-1/Assignment-operator.py
+++ Class-1/Assignment-operator.py
@@ -5,9 +5,9 @@ print(c)
 
 print("before value of a is ", str(a))  # 10
 a += 2  # 10 + 1  = a (11)
-# a = a + 1
+a = a + 1
 print("after value of a is ", str(a))  # 20
-# print(a)
+print(a)
 
-# a -= 10
-# print(a)
+a -= 10
+print(a)
--- Class-5/pandas-dataframe.py
+++ Class-5/pandas-dataframe.py
@@ -1,8 +1,12 @@
 import numpy as np
 import pandas as py
 
+#filename = "Cass-5/weather.csv"
+#def getDataFromFile(filename):
+   # if filename:
+       # df = py.read_csv(filename)
+       # print(df)
 
-def getDataFromFile(filename):
-    if filename:
-        df = py.read_csv(filename)
-        print(df)
+filename = "Cass-5/weather.csv"
+df = py.read_csv(filename)
+print(df)
